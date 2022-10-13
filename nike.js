2fc678b84509bf973b7d0bf42800d1292b085cc2
Wilfred007 <114983732+Wilfred007@users.noreply.github.com>
2022-10-13 23:59:58 +0100

    Add files via upload

---
+++ nike.js
@@ -0,0 +1,10 @@
+
+GamepadButton.
+var loginInto;
+if (confirm("Do you want to save changes?")== true) {
+    loginInto = "Data saved successfully!";
+} else {
+    loginInto = "Save Canceled! ";
+
+}
+
